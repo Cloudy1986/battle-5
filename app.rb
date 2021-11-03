87ce7e52aa8235668728a89f26078588e5dd3cf7
Mark McLeod <markmcleod45@gmail.com>
2021-11-03 18:06:40 +0000

    End of section: We can enter two names via a form at the / route and then see the names at the /names route

--- app.rb
+++ app.rb
@@ -11,9 +11,8 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    p params
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+    @player_1_name = params["player_1_name"]
+    @player_2_name = params["player_2_name"]
     erb(:play)
   end
 
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -4,6 +4,9 @@ feature 'Enter names' do
     fill_in :player_1_name, with: 'Mark'
     fill_in :player_2_name, with: 'Dave'
     click_button 'Submit'
+
+    save_and_open_page # saves the web page and opens the browser to display it
+
     expect(page).to have_content 'Mark vs. Dave'
   end
 end
