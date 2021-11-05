85a88e0b4a280277d9f62b4a7b9822c339123876
Mark McLeod <markmcleod45@gmail.com>
2021-11-05 09:24:58 +0000

    Remove enable sessions because no longer using sessions

--- app.rb
+++ app.rb
@@ -7,22 +7,21 @@ class Battle < Sinatra::Base
   configure :development do
     # register Sinatra::Reloader
   end
-  enable :sessions
 
   get '/' do
     erb(:index)
-  end
-
-  get '/play' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
-    erb(:play)
   end
 
   post '/names' do
     $player_1 = Player.new(params[:player_1_name])
     $player_2 = Player.new(params[:player_2_name])
     redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
+    erb(:play)
   end
 
   # start the server if ruby file executed directly
